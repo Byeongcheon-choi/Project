56af4dc75ae43c7a511a67ff5afbbdb4a0dcd51c
Byeongcheon-choi <69423802+Byeongcheon-choi@users.noreply.github.com>
2020-08-10 18:39:53 -0400

    Add files via upload
    Make UI first.

---
+++ Piano Game/project/src/project/UI.java
@@ -0,0 +1,45 @@
+package project;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+
+public class UI extends JFrame{
+		
+		public static final int SCREEN_WIDTH =1280;
+		public static final int SCREEN_HEIGHT = 720;
+		private Image screenImage;
+		private Graphics screenGraphic;
+		
+		private Image introBackground;
+		
+		public UI()
+		{
+			setTitle("Piano Game");
+			setSize(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+			setResizable(false);
+			setLocationRelativeTo(null);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setVisible(true);
+			
+			introBackground = new ImageIcon(Main.class.getResource("../Backeffect/backback.jpg")).getImage();
+		}
+		
+		public void paint(Graphics g)
+		{
+			screenImage = createImage(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+			screenGraphic = screenImage.getGraphics();
+			screenDraw(screenGraphic);
+			g.drawImage(screenImage,0,0,null);
+		}
+		
+		public void screenDraw(Graphics g)
+		{
+			g.drawImage(introBackground,0,0,null);
+			this.repaint();
+			
+		}
+
+}
